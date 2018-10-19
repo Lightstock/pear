9e71f024752b79859b2c49ceea061f6b429bcda5
Derik Olsson <do@derik.co>
2018-10-19 11:19:43 -0500

    Option to manually exclude people

--- pear.rb
+++ pear.rb
@@ -1,21 +1,29 @@
 #!/usr/bin/env ruby
 
 require 'csv'
+require 'optparse'
+
+options = {}
+OptionParser.new do |opts|
+  opts.on('-e', '--exclude STRING,STRING', 'Exclude names from this run') do |x|
+    options[:exclude] = x.split(',')
+  end
+end.parse!
 
 if ARGV[-1].nil?
-  puts 'Usage: pear.rb input.csv'
+  puts 'Usage: pear.rb [options] input.csv'
   exit
 end
 
-input_file = ARGV[-1].strip
+options[:in] = ARGV[-1].strip
 
-unless input_file =~ /.*\.csv$/i
-  puts "ERROR: CSVs only (input: #{input_file})"
+unless options[:in] =~ /.*\.csv$/i
+  puts "ERROR: CSVs only (input: #{options[:in]})"
   exit
 end
 
 people = {}
-CSV.foreach(input_file, headers: true, header_converters: :symbol) do |row|
+CSV.foreach(options[:in], headers: true, header_converters: :symbol) do |row|
   unless row.headers.include? :name
     puts 'CSV must have a header row, with one of the columns being "Name"'
     puts "Headers: #{row.headers&.to_a&.join(', ')}"
@@ -31,6 +39,13 @@ off_last_time = people.select { |_k, v| v[:last_paired_with].nil? }.keys.shuffle
 off_last_time.each do |name|
   @names.delete(name)
   @names.unshift(name)
+end
+
+options[:exclude]&.each do |name|
+  if @names.delete(name)
+    people[name][:last_paired_with] = nil
+    puts "#{name}: Excluded"
+  end
 end
 
 pairs_count = @names.length / 2
@@ -56,7 +71,7 @@ unless @names.length.zero?
   puts "#{person[:name]}: Bye Week"
 end
 
-CSV.open(input_file, 'w') do |csv|
+CSV.open(options[:in], 'w') do |csv|
   csv << %w[name last_paired_with]
   people.each_value do |p|
     csv << [p[:name], p[:last_paired_with]]
