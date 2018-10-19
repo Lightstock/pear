371be378edc047f599589b017829556dfbec291c
Derik Olsson <do@derik.co>
2018-10-19 10:39:56 -0500

    Pairs people together, writes out to STDOUT, and stores to CSV

--- pear.rb
+++ pear.rb
@@ -14,13 +14,45 @@ unless input_file =~ /.*\.csv$/i
   exit
 end
 
-csv = CSV.open(input_file, 'r+', headers: true, header_converters: :downcase)
-rows = csv.read
+people = {}
+CSV.foreach(input_file, headers: true, header_converters: :symbol) do |row|
+  unless row.headers.include? :name
+    puts 'CSV must have a header row, with one of the columns being "Name"'
+    puts "Headers: #{row.headers&.to_a&.join(', ')}"
+    exit
+  end
 
-unless csv.headers.include? 'name'
-  puts 'CSV must have a header row, with one of the columns being "Name"'
-  puts "Headers: #{csv.headers&.to_a&.join(', ')}"
-  exit
+  people[row[:name]] = { name: row[:name],
+                         last_paired_with: row[:last_paired_with] }
 end
 
-puts rows
+@names = people.keys.shuffle
+pairs_count = @names.length / 2
+
+pairs_count.times do
+  person = people[@names.shift]
+  available_matches = @names.reject { |name| name == person[:last_paired_with] }
+  pair = available_matches.sample
+  person[:last_paired_with] = pair
+  people[pair][:last_paired_with] = person[:name]
+  @names.delete(pair)
+
+  puts "#{person[:name]} & #{person[:last_paired_with]}"
+
+  pairs_count -= 1
+end
+
+unless @names.length.zero?
+  person = people[@names.shift]
+
+  people[person[:name]][:last_paired_with] = nil
+
+  puts "#{person[:name]}: Bye Week"
+end
+
+CSV.open(input_file, 'w') do |csv|
+  csv << %w[name last_paired_with]
+  people.each_value do |person|
+    csv << [person[:name], person[:last_paired_with]]
+  end
+end
